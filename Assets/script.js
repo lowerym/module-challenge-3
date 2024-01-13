7e4f9b43c7ecfa4626d29c7a2c0db75dee84c81d
Matthew Lowery <matthewdlowery@msn.com>
2024-01-13 15:27:16 -0600

    Restructed files and relinked files in index file

--- Assets/script.js
+++ Assets/script.js
@@ -5,7 +5,7 @@ function generatePassword() {
   var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   var lowerCase = "abcdefghijklmnopqrstuvwxyz";
   var numbers = "0123456789";
-  var specialCharacters = " !#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+  var specialCharacters = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
   var multiChoice = "";
   var password = "";
 
